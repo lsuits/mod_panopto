b3c230b65e0b32467d4063ff427c2cd04cde9b73
Robert Russo <rrusso@lsu.edu>
2015-07-14 14:24:25 -0500

    Initial commit of the uber awesome Panopto Mod

---
+++ lang/en/panopto.php
@@ -0,0 +1,69 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'panopto', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package    mod_panopto
+ * @copyright  1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['clicktoopen'] = 'Click {$a} link to open resource.';
+$string['configdisplayoptions'] = 'Select all options that should be available, existing settings are not modified. Hold CTRL key to select multiple fields.';
+$string['configframesize'] = 'When a web page or an uploaded file is displayed within a frame, this value is the height (in pixels) of the top frame (which contains the navigation).';
+$string['configauthinstance'] = 'The name of the authentication instance set within your Panopto server.';
+$string['configpanoptoserver'] = 'The URL of your Panopto server without a trailing slash.';
+$string['contentheader'] = 'Content';
+$string['createpanopto'] = 'Create a Panopto link on your course';
+$string['displayoptions'] = 'Available display options';
+$string['displayselect'] = 'Display';
+$string['displayselect_help'] = 'This setting, and whether the browser allows embedding, determines how the Panopto is displayed. Options may include:
+
+* Automatic - The best display option for the Panopto is selected automatically
+* Embed - The Panopto is displayed within the page below the navigation bar together with the Panopto description and any blocks
+* Open - Only the Panopto is displayed in the browser window
+* In pop-up - The Panopto is displayed in a new browser window without menus or an address bar
+* In frame - The Panopto is displayed within a frame below the navigation bar and Panopto description
+* New window - The Panopto is displayed in a new browser window with menus and an address bar';
+$string['displayselectexplain'] = 'Choose display type, unfortunately not all types are suitable for all Panoptos.';
+$string['externalpanopto'] = 'Panopto URL';
+$string['framesize'] = 'Frame height';
+$string['invalidstoredpanopto'] = 'Cannot display this resource, Panopto is invalid.';
+$string['invalidpanopto'] = 'Entered Panopto is invalid';
+$string['modulename'] = 'Panopto';
+$string['modulename_help'] = 'The Panopto module enables a teacher to provide a web link as a course resource. Anything that is freely available online, such as documents or images, can be linked to; the Panopto doesn’t have to be the home page of a website. The Panopto of a particular web page may be copied and pasted or a teacher can use the file picker and choose a link from a repository such as Flickr, YouTube or Wikimedia (depending upon which repositories are enabled for the site).
+
+There are a number of display options for the Panopto, such as embedded or opening in a new window and advanced options for passing information, such as a student\'s name, to the Panopto if required.
+
+Note that Panoptos can also be added to any other resource or activity type through the text editor.';
+$string['modulename_link'] = 'mod/panopto/view';
+$string['modulenameplural'] = 'Panoptos';
+$string['page-mod-panopto-x'] = 'Any Panopto module page';
+$string['pluginadministration'] = 'Panopto module administration';
+$string['pluginname'] = 'Panopto';
+$string['popupheight'] = 'Pop-up height (in pixels)';
+$string['popupheightexplain'] = 'Specifies default height of popup windows.';
+$string['popupwidth'] = 'Pop-up width (in pixels)';
+$string['popupwidthexplain'] = 'Specifies default width of popup windows.';
+$string['printintro'] = 'Display Panopto description';
+$string['printintroexplain'] = 'Display Panopto description below content? Some display types may not display description even if enabled.';
+$string['authinstance'] = 'Authentication Instance';
+$string['panoptoserver'] = 'Panopto Server';
+$string['serverpanopto'] = 'Server Panopto';
+$string['panopto:addinstance'] = 'Add a new Panopto resource';
+$string['panopto:view'] = 'View Panopto';
